c2badaf730a69c710c92003b4d110e827275bcba
Raiden Freeman <raidenfreeman@hotmail.com>
2017-10-07 21:24:36 +0300

    Added debug module, and debug config for VS code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const debug = require('debug')('ln');
 
 // Do work here
 router.get('/', (req, res) => {
+  debug('%O',req);
   res.send('Hey! It works!');
 });
 
